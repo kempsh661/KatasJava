54af4fcc91cc2ac25a3e5ca22c5d46cd572107b0
Laura <laura.barona@uao.edu.co>
2026-06-26 10:09:43 -0500

    Kata 1

--- src/katas/Kata1.java
+++ src/katas/Kata1.java
@@ -11,6 +11,19 @@ public class Kata1 {
         double totalCuenta = sc.nextDouble();
 
         System.out.println("Ingresa el porcentaje de propina");
+        double porcentajePropina = sc.nextDouble();
+
+        double propina = totalCuenta * porcentajePropina / 100;
+        double totalPagar = totalCuenta + propina;
+
+        System.out.println("Propina: $" + propina);
+        System.out.println("Total a pagar: $" + totalPagar);
+
+        if(porcentajePropina > 30) {
+            System.out.println("¡Propina generosa!");
+        } else if (porcentajePropina > 5) {
+            System.out.println("Propina muy baja");
+        }
 
     }
 }
