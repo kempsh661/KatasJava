dca98d10c4f9cecc478ede513b957d1644e3308b
Laura <laura.barona@uao.edu.co>
2026-06-26 09:49:22 -0500

    Adding Kata1

---
+++ src/katas/Kata1.java
@@ -0,0 +1,16 @@
+package katas;
+
+import java.util.Scanner;
+
+public class Kata1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa el total de la cuenta: ");
+        double totalCuenta = sc.nextDouble();
+
+        System.out.println("Ingresa el porcentaje de propina");
+
+    }
+}
